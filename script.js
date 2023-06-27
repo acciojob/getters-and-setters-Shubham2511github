be9119139dd545cd89a2c1478efac1b7079097fc
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-27 19:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
+	}
+	get name(){
+		return this._name;
+	}
+	get age(){
+		return this._age;
+	}
+	set age(value){
+		return this._age = value;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this._name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this._name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
